0cf2c78c64e6984d9ad1bd8df11b411a84476da5
AlexFagschlunger <alfagschlunger@tsn.at>
2020-01-29 09:29:04 +0100

    First commit

---
+++ Website_FcBarcelona/Website_FcBarcelona/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website_FCBarcelona.Models;
+
+namespace Website_FCBarcelona.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Team()
+        {
+            Mannschaft p2 = new Mannschaft(1, "Lionel", "Messi", new DateTime(1987, 6, 24), 152.5m, "FW");
+            Mannschaft p3 = new Mannschaft(2, "Ansu", "Fati", new DateTime(2002, 10, 31), 42.5m, "RW");
+            Mannschaft p4 = new Mannschaft(3, "Frenkie", "De Jong", new DateTime(1997, 5, 12), 83.5m, "CM");
+            Mannschaft p5 = new Mannschaft(4, "Marc-Andre", "Ter Stegen", new DateTime(1992, 4, 30), 94.0m, "GK");
+
+            List<Mannschaft> p1 = new List<Mannschaft>();
+            p1.Add(p2);
+            p1.Add(p3);
+            p1.Add(p4);
+            p1.Add(p5);
+
+            return View(p1);
+        }
+    }
+}
---
+++ Website_FcBarcelona/Website_FcBarcelona/Models/Mannschaft.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website_FCBarcelona.Models
+{
+    public class Mannschaft
+    {
+            public int Number { get; set; }
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+            public DateTime Birthdate { get; set; }
+            public decimal Salary { get; set; }
+            public string Position { get; set; }
+
+            public Mannschaft() : this(0, "", "", DateTime.MinValue, 0.0m, "") { }
+
+            public Mannschaft(int number, string firstname, string lastname, DateTime birthdate, decimal salary, string position)
+            {
+                this.Number = number;
+                this.Firstname = firstname;
+                this.Lastname = lastname;
+                this.Birthdate = birthdate;
+                this.Salary = salary;
+                this.Position = position;
+            }
+
+            // ToString ...
+
+        }
+    }
